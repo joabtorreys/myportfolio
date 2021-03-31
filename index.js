799f322dde508c3885ce5d28c55567c6412b281d
joab <j.torreys@outlook.com>
2021-03-31 11:13:10 -0800

    mobile menue working

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+const burger = document.querySelector("#burger");
+const headernav = document.querySelector(".header-nav");
+burger.addEventListener("click", () => {
+  headernav.classList.toggle("active");
+});
